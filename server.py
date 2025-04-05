b9219634b9d0a4b1155d5196215f2d211a2f8671
umairz5723 <108788149+umairz5723@users.noreply.github.com>
2025-04-05 00:50:10 -0400

    Update server.py

--- server.py
+++ server.py
@@ -40,4 +40,4 @@ def get_dijkstra():
     return process_search(dijkstra, generate_dijkstra_maze)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
